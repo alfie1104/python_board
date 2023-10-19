20d72b19a496f05bd48f4d86ccbe4c163fef07c0
peter_alfie <alfie1104@gmail.com>
2023-10-19 15:44:23 +0900

    Declare model

---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+
+class Question(Base):
+    __tablename__ = "question"
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+
+
+class Answer(Base):
+    __tablename__ = "answer"
+
+    id = Column(Integer, primary_key=True)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
+    question_id = Column(
+        Integer, ForeignKey("question.id")
+    )  # question.id : question테이블의 id칼럼을 의미 (Question 객체를 의미하는게 아님)
+    question = relationship(
+        "Question", backref="answers"
+    )  # Question : 참조할 모델명, backref : 역참조 설정
