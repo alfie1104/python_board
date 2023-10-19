1a7f40c14ca8aabcf8c9df64f6433253f2b9d98a
peter_alfie <alfie1104@gmail.com>
2023-10-19 15:27:31 +0900

    setting up database

--- database.py
+++ database.py
@@ -4,6 +4,12 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./myapi.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)  # create_engine은 커넥션 풀을 생성함
 
-SessionLocal
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)  # 데이터베이스에 접속하기 위해 필요한 클래스
+
+Base = declarative_base()  # 반환된 Base는 데이터 베이스 모델을 구성할 때 활용함
