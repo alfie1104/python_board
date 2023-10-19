21cd60597000c98a07b1652fe8e7cc542bb21d7f
peter_alfie <alfie1104@gmail.com>
2023-10-19 16:57:55 +0900

    implement get_db function to open and return session automatically

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+import contextlib
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -13,3 +15,25 @@ SessionLocal = sessionmaker(
 )  # 데이터베이스에 접속하기 위해 필요한 클래스
 
 Base = declarative_base()  # 반환된 Base는 데이터 베이스 모델을 구성할 때 활용함
+
+
+"""
+[get_db]
+ 참고 : contextmanager  -> https://docs.python.org/ko/3/library/contextlib.html
+ db 세션 객체를 리턴하는 제너레이터(이터레이터를 생성해주는 함수)인 get_db 함수
+ 제너레이터 함수에 @contextlib.contextmanager 어노테이션을 적용했으므로 다음과 같이 with문과 함께 쓸 수 있음
+
+ with get_db() as db:
+   # db 세션 객체를 사용
+
+ with문을 벗어나는 순간 get_db 함수의 finally에 작성한 db.close() 함수가 자동으로 실행됨
+"""
+
+
+@contextlib.contextmanager
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -1,14 +1,24 @@
 from fastapi import APIRouter
 
-from database import SessionLocal
+# from database import SessionLocal
+from database import get_db
 from models import Question
 
 router = APIRouter(prefix="/api/question")
 
 
+# @router.get("/list")
+# def question_list():
+#     db = SessionLocal()  # db 세션 생성
+#     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+#     db.close()  # 사용한 세션을 커넥션풀에 반환 (세션을 종료하는것이 아님)
+#     return _question_list
+
+
+# get_db를 이용하여 세션 생성 및 반환하도록 개선 수정
 @router.get("/list")
 def question_list():
-    db = SessionLocal()  # db 세션 생성
-    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
-    db.close()  # 사용한 세션을 커넥션풀에 반환 (세션을 종료하는것이 아님)
+    with get_db() as db:  # db 세션 생성 및 사용완료시 반환(db.close()가 실행됨)
+        _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+
     return _question_list
