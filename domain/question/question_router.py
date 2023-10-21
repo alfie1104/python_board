43dcbf37fb7ddfe3d3893f3c13bbe9f291ef3e04
peter_alfie <alfie1104@gmail.com>
2023-10-22 00:28:32 +0900

    Processing paging

--- domain/question/question_router.py
+++ domain/question/question_router.py
@@ -37,11 +37,13 @@ router = APIRouter(prefix="/api/question")
 
 
 # @router.get("/list")
-@router.get("/list", response_model=list[question_schema.Question])  # pydantic 스키마 적용
-def question_list(db: Session = Depends(get_db)):
-    _question_list = question_crud.get_question_list(db)
+@router.get("/list", response_model=question_schema.QuestionList)  # pydantic 스키마 적용
+def question_list(db: Session = Depends(get_db), page: int = 0, size: int = 10):
+    total, _question_list = question_crud.get_question_list(
+        db, skip=page * size, limit=size
+    )
 
-    return _question_list
+    return {"total": total, "question_list": _question_list}
 
 
 @router.get("/detail/{question_id}", response_model=question_schema.Question)
--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -3,14 +3,25 @@
   import {link} from "svelte-spa-router"
 
     let question_list = []
+    let size = 10
+    let page = 0
+    let total = 0
+    $: total_page = Math.ceil(total/size) //svelte에서 변수앞에 $: 기호를 붙이면 해당 변수는 반응형변수가 됨. total의 값이 바뀌면 total_page의 값도 실시간으로 재계산됨
 
-    function get_question_list() {
-        fastapi('get','/api/question/list',{},(json) => {
-            question_list = json
+    function get_question_list(_page) {
+        let params = {
+            page : _page,
+            size : size
+        }
+
+        fastapi('get','/api/question/list',params,(json) => {
+            question_list = json.question_list
+            page = _page
+            total = json.total
         })
     }
 
-    get_question_list()
+    get_question_list(0)
     
 </script>
 
@@ -35,6 +46,27 @@
             {/each}
         </tbody>
     </table>
+
+    <!-- 페이징처리 시작-->
+    <ul class="pagination justify-content-center">
+        <!--이전 페이지-->
+        <li class="page-item {page <= 0 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list(page-1)}">이전</button>
+        </li>
+        <!-- 페이지 번호-->
+        {#each Array(total_page) as _, loop_page}
+        {#if loop_page >= page-5 && loop_page <= page+5}
+        <li class="page-item {loop_page === page && 'active'}">
+            <button on:click="{()=>get_question_list(loop_page)}" class="page-link">{loop_page + 1}</button>
+        </li>
+        {/if}
+        {/each}
+        <!-- 다음 페이지-->
+        <li class="page-item {page >= total_page -1 && 'disabled'}">
+            <button class="page-link" on:click="{() => get_question_list(page+1)}">다음</button>
+        </li>
+    </ul>
+    <!-- 페이징 처리 끝-->
 
     <a use:link href="/question-create" class="btn btn-primary">질문 등록하기</a>
 </div>
